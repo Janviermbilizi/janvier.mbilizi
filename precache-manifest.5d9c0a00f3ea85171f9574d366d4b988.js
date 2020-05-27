b958dd0cb9505c88a0441536f8acda45263872eb
Admin <admin@Admins-MacBook-Pro.local>
2020-05-26 23:01:57 -0600

    Updates

---
+++ precache-manifest.5d9c0a00f3ea85171f9574d366d4b988.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0659193d118393d40fa81abfbe0b972c",
+    "url": "/janvier.mbilizi/index.html"
+  },
+  {
+    "revision": "2ed846c26cb5d420775f",
+    "url": "/janvier.mbilizi/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "47e6d2bf7ad321c41891",
+    "url": "/janvier.mbilizi/static/js/2.513869a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/janvier.mbilizi/static/js/2.513869a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed846c26cb5d420775f",
+    "url": "/janvier.mbilizi/static/js/main.78445a87.chunk.js"
+  },
+  {
+    "revision": "e28f5d92428bc4d3ff05",
+    "url": "/janvier.mbilizi/static/js/runtime-main.68fdc275.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/janvier.mbilizi/static/media/logo.5d5d9eef.svg"
+  }
+]);
